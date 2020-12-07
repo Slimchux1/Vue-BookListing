1c3293de360e4890a894e885a8d4097ed0b0bb28
Slimchux1 <38258308+Slimchux1@users.noreply.github.com>
2020-12-07 18:36:32 +0100

    Add files via upload
    initial commit

---
+++ src/components/BookItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul>
+       
+        <li v-for="books in book">
+             {{ books.title }}: {{books.author}}
+             </li>
+
+    </ul>
+</template>
+
+
+<script>
+export default {
+    name: 'BookItem',
+    props: ['book']
+    
+}
+</script>
+
+
+
+<style  scoped>
+li {
+   display: block;
+   margin: 0 10px;
+}
+</style>
---
+++ src/components/BookList.vue
@@ -0,0 +1,51 @@
+<template>
+   <div>
+     <h1>{{ title }}</h1>
+     <BookItem v-bind:book="books"></BookItem>
+  
+   <book-form @addBook='appendBook'></book-form>
+   </div>
+</template>
+
+<script>
+import BookItem from './BookItem';
+import BookForm from './BookForm';
+
+export default {
+  name: "BookList",
+   components: {
+   'BookItem': BookItem,
+   'BookForm': BookForm
+  },
+  data() {
+   return {
+      title: "All Books",
+      books: [
+            {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+            {title: 'American Gods', author: 'Neil Gaiman'},
+             {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+      ]
+   } 
+   },
+   methods: {appendBook(bookTitle, bookAuthor) {
+       this.books.push({title: bookTitle, author: bookAuthor })
+   }
+   }
+};
+</script>
+
+
+<style>
+h1, h2 {
+   font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+   display: block;
+   margin: 0 10px;
+}
+</style>
